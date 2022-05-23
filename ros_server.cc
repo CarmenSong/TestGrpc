f7e5884810cd32a9e2b2146128f1d55a8336200d
CarmenSong <102047534+CarmenSong@users.noreply.github.com>
2022-05-23 14:30:26 +0900

    Add files via upload

---
+++ ros_server.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+
+#include "TestRos.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using TestRos::Turtlesim;
+using TestRos::TurtleRequest;
+using TestRos::TurtleReply;
+
+class TestRosService : public Turtlesim::Service {
+        Status MoveTurtle(ServerContext* context, const TurtleRequest* request,
+        TurtleReply* reply) {
+                reply->set_message(request->name());
+                return Status::ok;
+        } 
+};
+
+void RunServer() {
+        std::string server_address("localhost:9515");
+        TestRosService service;
+
+        grpc::EnableDefaultHealthCheckService(true);
+        grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+        
+        ServerBuilder builder;
+        builder.AddListeningService(&service);
+        builder.RegisterService(&service);
+
+        std::unique_ptr<Server> server(builder.BuildAndStart());
+        std::cout << "Server listening on " << server_address << std::endl;
+
+        server->Wait();
+}
+
+int main(int argc, char** argv) {
+        RunServer();
+        
+        return 0;
+}
