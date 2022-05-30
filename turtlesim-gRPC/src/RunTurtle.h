a350fa5eed7a94f9c749b93c2f12b4d43d15028a
CarmenSong <102047534+CarmenSong@users.noreply.github.com>
2022-05-31 00:00:03 +0900

    Create RunTurtle.h

---
+++ turtlesim-gRPC/src/RunTurtle.h
@@ -0,0 +1,15 @@
+void RunTurtle() {
+        
+        auto node = rclcpp::Node::make_shared("turtle_controller");
+        auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel",1);
+
+        rclcpp::WallRate loop(5);
+        while (rclcpp::ok()) {
+                auto msg = geometry_msgs::msg::Twist();
+                msg.linear.x = 1.0;
+                msg.angular.z = 1.0;
+                publisher->publish(msg);
+                loop.sleep();
+        }
+
+}
