f8bc0188938695015632b5f2d3d387ecbe91a6a9
CarmenSong <102047534+CarmenSong@users.noreply.github.com>
2022-05-31 00:01:21 +0900

    Update ros_server.cc

--- turtlesim-gRPC/src/ros_server.cc
+++ turtlesim-gRPC/src/ros_server.cc
@@ -11,6 +11,8 @@
 
 #include "TestRos.grpc.pb.h"
 
+#include "RunTurtle.h"
+
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
@@ -27,22 +29,6 @@ class TestRosService : public Turtlesim::Service {
                 return Status::OK;
         } 
 };
-
-void RunTurtle() {
-        
-        auto node = rclcpp::Node::make_shared("turtle_controller");
-        auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel",1);
-
-        rclcpp::WallRate loop(5);
-        while (rclcpp::ok()) {
-                auto msg = geometry_msgs::msg::Twist();
-                msg.linear.x = 1.0;
-                msg.angular.z = 1.0;
-                publisher->publish(msg);
-                loop.sleep();
-        }
-
-}
 
 void RunServer() {
 
