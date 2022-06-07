437531982b69450e30453e673969b1f54603911e
CarmenSong <102047534+CarmenSong@users.noreply.github.com>
2022-06-07 09:40:40 +0900

    Update mqtt_pub.cpp

--- mqtt/mqtt_pub.cpp
+++ mqtt/mqtt_pub.cpp
@@ -1,34 +1,66 @@
-//#include <stdio>
-#include <mosquitto.h>
-#include <string>
 #include <iostream>
+#include <fstream>
+#include <memory>
+#include <vector>
+#include "Message.pb.h"
+#include "mosquitto.h"
 
-int main() {
-        int rc;
-        struct mosquitto* mosq;
+namespace mesg {
 
-        mosquitto_lib_init();
+class MessageHandler {
+public:
 
+    void init() {
         mosq = mosquitto_new("publisher-test", true, NULL);
 
         rc = mosquitto_connect(mosq, "localhost", 1883, 60);
         if (rc!=0) {
-                std::cout << "Cannot connect to broker " << rc <<  std::endl;
-                mosquitto_destroy(mosq);
-                return -1;
-        }
-        std::cout << "Broker Connectiong Success! \n" << std::endl;;
-        
-        
-        std::string mqtt_msg;
-        std::cin >> mqtt_msg;
-        const void* msg_pub = mqtt_msg.c_str();
-        mosquitto_publish(mosq, NULL, "test/t1", sizeof(mqtt_msg), msg_pub, 0, false);
+        std::cout << "Cannot connect to broker " << rc <<  std::endl;
+        mosquitto_destroy(mosq);
+        return;
+    }
+    std::cout << "Broker Connection Success" << std::endl;
+    }
 
+    int pub_message(const std::string& m) {
+
+        Message message;
+        message.set_body(m);
+        //message.mutable_body()->add_context(m);
+
+        std::string message_string;
+        message.SerializeToString(&message_string); //Serializes the message and stroes in a given string
+        //std::cout <<  message_string << std::endl;
+        mosquitto_publish(mosq, nullptr, mesg_pub_topic.c_str(),
+                          static_cast<int>(message_string.length()+1),
+                          message_string.c_str(), 0, false); 
+        
         mosquitto_disconnect(mosq);
-        mosquitto_destroy(mosq); // メモリ開放
+        mosquitto_destroy(mosq);
 
         mosquitto_lib_cleanup();
+        
         return 0;
+    }
 
+private:
+    struct mosquitto* mosq;
+    int rc;
+    std::string mesg_pub_topic = "MessageHandler/publish";
+};
+}
+ 
+int main(int argc, char *argv[]) {
+
+    mosquitto_lib_init();
+    
+    mesg::MessageHandler message;
+
+    message.init();
+    message.pub_message("test");
+
+    // Optional:  Delete all global objects allocated by libprotobuf.
+    google::protobuf::ShutdownProtobufLibrary();
+ 
+    return 0;
 }
