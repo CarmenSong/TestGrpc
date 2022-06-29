c0ad97cea64533f126b9702f09f25ef143f958a2
CarmenSong <102047534+CarmenSong@users.noreply.github.com>
2022-06-29 13:50:43 +0900

    Update main.cpp

--- dynamic_ros/src/main.cpp
+++ dynamic_ros/src/main.cpp
@@ -14,33 +14,26 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
-bool file_update = false;
-
-void lib_update(std::string initial_ft, std::filesystem::file_time_type ft) {
+void check_lib_update(std::string initial_ft, std::filesystem::file_time_type ft) {
 
         while (rclcpp::ok()) {
 
-                bool if_file_update = if_lib_update(initial_ft, ft);
-                std::cout << initial_ft << std::endl;
+                bool dirc_update = if_dirc_update(initial_ft, ft);
 
-                if (if_file_update) {
-                
-                std::cout << "Library is updated" << std::endl;
-                if_file_update = false;
-                ft = std::filesystem::last_write_time("/home/jessewl/work/C-Programs/dynamic_ros/build/test/libdlopenRos.so");
-                initial_ft = get_filetime(ft);
-                file_update = true;
-                //thrd2.join();
-                //thrd2(loading_rosnode);
-
+                if (dirc_update) {
+                        
+                        ft = std::filesystem::last_write_time(Lib_PATH);
+                        initial_ft = get_dirctime(ft);
+                        lib_management();
                 }
-
                 sleep(1);
         }
 }
 
 void loading_rosnode() {
-        const auto dlopenRos = dlopen ("./libdlopenRos.so", RTLD_NOW);
+        char* lib_path = const_cast<char*>(Lib_container[Lib_container.size()-1].c_str());
+        //std::cout << Lib_container[0] << std::endl;
+        const auto dlopenRos = dlopen (lib_path, RTLD_NOW);
         auto Moveturtle = reinterpret_cast<void(*)(int, int)>(dlsym(dlopenRos, "_Z9Runturtleii"));
         Moveturtle(1,1);
         dlclose(dlopenRos);
@@ -50,26 +43,31 @@ int main (int argc, char **argv) {
 
         rclcpp::init(argc, argv);
 
-        std::filesystem::file_time_type filetime = std::filesystem::last_write_time("/home/jessewl/work/C-Programs/dynamic_ros/build/test/libdlopenRos.so");
-        std::string initial_filetime = get_filetime(filetime);
+        std::filesystem::file_time_type filetime = std::filesystem::last_write_time(Lib_PATH);
+        std::string initial_filetime = get_dirctime(filetime);
 
-        std::thread thrd1(lib_update, initial_filetime, filetime);
         std::thread thrd2(loading_rosnode);
+        std::thread thrd1(check_lib_update, initial_filetime, filetime);
+        
 
         while(rclcpp::ok()) {
-                if (file_update) {
-
-                thrd2.detach();
+                if (roslib_update) {
+                
+                thrd2.join();
                 std::cout << "Update Success" << std::endl;
-                file_update = false;
+                roslib_update = false;
                 break;
+                //std::thread thrd3(loading_rosnode);
+                //thrd3.join();
                 }
         }
+        //thrd2.join();
         
-        std::thread thrd3(loading_rosnode);
+
+        std::cout << "log" << std::endl; //False
 
         thrd1.join();
-        thrd3.join();
+        
 
         rclcpp::shutdown();
 
