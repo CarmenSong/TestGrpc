2761aa5b091194e0c1a32459a3a2cf064fe5b28e
CarmenSong <102047534+CarmenSong@users.noreply.github.com>
2022-07-05 10:14:25 +0900

    Update Runturtle.cpp

--- dynamic_test/src/Runturtle.cpp
+++ dynamic_test/src/Runturtle.cpp
@@ -9,26 +9,26 @@ void Runturtle(int a, int b) {
         auto node = rclcpp::Node::make_shared("turtle_controller");
         auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel",1);
         int count = 0;
-        //lib_management();
+        lib_management();
         int Initial_Lib_Number = Lib_container.size();
+        std::cout << "Initial Lib Number: " << Initial_Lib_Number << std::endl;
 
-        while (rclcpp::ok() && count < 100) {
+        while (rclcpp::ok()) {
                 auto msg = geometry_msgs::msg::Twist();
                 msg.linear.x = a;
                 msg.angular.z = b;
                 publisher->publish(msg);
-                //count++;
                 lib_management();
                 int Lib_Number = Lib_container.size();
-                //std::cout << Initial_Lib_Number << std::endl;
+
                 if (Initial_Lib_Number != Lib_Number) {
-                        std::cout << Lib_container.size() << std::endl;
+                        //std::cout << Lib_container.size() << std::endl;
                         std::cout << "Sth are coming!" << std::endl;
                         Initial_Lib_Number = Lib_Number;
                         char choice;
-                        std::cout << "Waiting thrd1 complete, then press enter" << std::endl;
+                        std::cout << "Waiting thrd1 complete, then enter Y" << std::endl;
                         std::cin >> choice;
-                        break;
+                        if (choice == 'Y') break;
                 }
 
         }
