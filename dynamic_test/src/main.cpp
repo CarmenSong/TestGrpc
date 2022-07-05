e27256f8d849108ed9073ef9f1eb4b8c5d441b85
CarmenSong <102047534+CarmenSong@users.noreply.github.com>
2022-07-05 10:14:43 +0900

    Update main.cpp

--- dynamic_test/src/main.cpp
+++ dynamic_test/src/main.cpp
@@ -25,12 +25,14 @@ void check_lib_update(std::string initial_ft, std::filesystem::file_time_type ft
                         ft = std::filesystem::last_write_time(Lib_PATH);
                         initial_ft = get_dirctime(ft);
                         lib_management();
+                        std::cout << "Update Complete" << std::endl;
                 }
                 sleep(1);
         }
 }
 
 void loading_rosnode() {
+        std::cout << "Using Lib: " << Lib_container[Lib_container.size()-1] << std::endl;
         char* lib_path = const_cast<char*>(Lib_container[Lib_container.size()-1].c_str());
         //std::cout << Lib_container[0] << std::endl;
         const auto dlopenRos = dlopen (lib_path, RTLD_NOW);
@@ -45,9 +47,7 @@ int main (int argc, char **argv) {
 
         std::filesystem::file_time_type filetime = std::filesystem::last_write_time(Lib_PATH);
         std::string initial_filetime = get_dirctime(filetime);
-        //std::thread thrd2(loading_rosnode);
         std::thread thrd1(check_lib_update, initial_filetime, filetime);
-        //std::thread thrd3(loading_rosnode);
 
         while(rclcpp::ok()) {
 
